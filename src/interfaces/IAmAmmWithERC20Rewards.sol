3a60e8b6eb9d8440c59f9651218443cb6873d534
DesiredDesire <konrad.wierzbik@gmail.com>
2024-12-06 09:12:45 +0100

    rename

--- src/interfaces/IAmAmmWithERC20Rewards.sol
+++ src/interfaces/IAmAmmWithERC20Rewards.sol
@@ -7,11 +7,11 @@ import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 
 /// @title amAMM With Cake Rewards Interface
 /// @notice This interface combines the Harberger Lease and Liquidity Per Second Tracker hooks
-/// to allow for bidding and distributing rewards to any pool with CAKE token.
+/// to allow for bidding and distributing rewards to any pool with any ERC20 token.
 /// @notice To be eligible for rewards, users must subscrbe to the pool IRewardTracker::INotifier.
 /// @notice IRewardTracker is used to track the liquidity per second of subscribed liquidity within pools like in V3.
 /// @notice The rewards are distributed to the subscribers based on the liquidity per second of the subscribed liquidity.
-interface IAmAmmWithCakeRewards is IArbiterAmAmmHarbergerLease, IRewardTracker {
+interface IAmAmmWithERC20Rewards is IArbiterAmAmmHarbergerLease, IRewardTracker {
     /// @notice Collects the rewards for the pool for the msg.sender
     /// @param key The key of the pool to collect rewards from
     function collectRewards(PoolKey calldata key) external;
