af8fb9a108e5f582f594ffc70002492e2bbb39c4
WookashWackomy <wookie.xp.07@gmail.com>
2024-12-06 18:19:57 +0100

    add tests

---
+++ test/contracts/NoOpRewardTracker.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
+import {Deployers} from "v4-core/test/utils/Deployers.sol";
+import {Constants} from "v4-core/test/utils/Constants.sol";
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {FullMath} from "v4-core/src/libraries/FullMath.sol";
+import {BalanceDelta, toBalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {PositionInfo, PositionInfoLibrary} from "v4-periphery/src/libraries/PositionInfoLibrary.sol";
+import {RewardTracker} from "../../src/RewardTracker.sol";
+import {PoolExtension} from "../../src/libraries/PoolExtension.sol";
+import {PositionExtension} from "../../src/libraries/PositionExtension.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {PosmTestSetup} from "v4-periphery/test/shared/PosmTestSetup.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "forge-std/console.sol";
+
+contract NoOpRewardTracker is BaseHook, RewardTracker {
+    using PoolExtension for PoolExtension.State;
+    using PositionExtension for PositionExtension.State;
+    using PoolIdLibrary for PoolKey;
+    using PositionInfoLibrary for PositionInfo;
+    using StateLibrary for IPoolManager;
+
+    constructor(
+        IPoolManager _poolManager,
+        IPositionManager _positionManager
+    ) BaseHook(_poolManager) RewardTracker(_positionManager) {}
+
+    function getHookPermissions() public pure virtual override returns (Hooks.Permissions memory) {
+        return
+            Hooks.Permissions({
+                beforeInitialize: true,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+    function beforeInitialize(address, PoolKey calldata key, uint160) external virtual override returns (bytes4) {
+        PoolId poolId = key.toId();
+        (, int24 tick, , ) = poolManager.getSlot0(poolId);
+        _initialize(poolId, tick);
+
+        return this.beforeInitialize.selector;
+    }
+
+    function afterSwap(
+        address,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) external virtual override onlyPoolManager returns (bytes4, int128) {
+        PoolId poolId = key.toId();
+        (, int24 tick, , ) = poolManager.getSlot0(poolId);
+        console.log("[NoOpRewardTracker.afterSwap] tick:", tick);
+
+        _changeActiveTick(poolId, tick, key.tickSpacing);
+
+        return (this.afterSwap.selector, 0);
+    }
+
+    function _beforeOnSubscribeTracker(PoolKey memory key) internal virtual override {
+        // console.log("beforeOnSubscribeTracker");
+    }
+    function _beforeOnUnubscribeTracker(PoolKey memory key) internal virtual override {
+        // console.log("beforeOnUnubscribeTracker");
+    }
+    function _beforeOnModifyLiquidityTracker(PoolKey memory key) internal override {}
+
+    function _beforeOnNotifyBurnTracker(PoolKey memory key) internal override {}
+
+    function donateRewards(PoolId poolId, uint128 amount) public {
+        _distributeReward(poolId, amount);
+    }
+
+    function accrueRewards(uint256 tokenId) public {
+        (PoolKey memory poolKey, PositionInfo positionInfo) = positionManager.getPoolAndPositionInfo(tokenId);
+        uint128 liquidity = positionManager.getPositionLiquidity(tokenId);
+
+        _accrueRewards(
+            tokenId,
+            IERC721(address(positionManager)).ownerOf(tokenId),
+            liquidity,
+            pools[poolKey.toId()].getRewardsPerLiquidityInsideX128(positionInfo.tickLower(), positionInfo.tickUpper())
+        );
+    }
+
+    function collectRewards(address to) external returns (uint256 rewards) {
+        rewards = accruedRewards[msg.sender];
+        accruedRewards[msg.sender] = 0;
+    }
+}
